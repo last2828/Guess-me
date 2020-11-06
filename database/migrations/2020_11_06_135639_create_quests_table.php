c12ec6c5a8fe677c9c0ff2b938efde4cb3fd5ff9
Kirill Buziuk <last2828@gmail.com>
2020-11-06 19:00:05 +0200

    update
    Update migration quests_table

--- database/migrations/2020_11_06_135639_create_quests_table.php
+++ database/migrations/2020_11_06_135639_create_quests_table.php
@@ -15,6 +15,16 @@ class CreateQuestsTable extends Migration
     {
         Schema::create('quests', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->unique();
+            $table->longText('description')->nullable();
+            $table->string('logo')->nullable();
+            $table->string('lead_time');
+            $table->integer('questions_count');
+            $table->integer('min_players_count');
+            $table->integer('max_players_count');
+            $table->string('difficulty_level');
+            $table->integer('user_id');
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
